88f956d078566c29c740297c3e99b281090780af
qmee <64937883+qmeehd@users.noreply.github.com>
2023-05-19 22:55:28 -0500

    Update Program.cs
    fixing comment

--- Testing/Program.cs
+++ Testing/Program.cs
@@ -64,7 +64,7 @@ namespace Testing
                     return;
 
             }
-            // PRINTS 
+            // PRINTS RESULT
             Console.WriteLine($"\nThe answer is {result}.");
         }
     }
